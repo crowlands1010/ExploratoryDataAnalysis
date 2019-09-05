369a3679e31ff50ee5efa50aa87530572ac38100
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-05 13:24:57 -0500

    Add files via upload
    plot1.R and plot1.png

--- Project 1/plot1.R
+++ Project 1/plot1.R
@@ -1,1 +1,26 @@
 library(datasets)
+## Download File
+txtFile <- "./household_power_consumption.txt"
+url       <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipFile  <- "./household_power_consumption.zip"
+
+# Check if the data is downloaded and download when applicable
+if (!file.exists("./household_power_consumption.txt")) {
+  download.file(url, destfile = zipFile)
+  unzip(zipFile)
+  file.remove(zipFile)
+}
+
+## Read File
+consumption <- read.table("household_power_consumption.txt", sep=";", 
+                        header=TRUE,na.strings="?")
+specConsumption <- consumption[(consumption$Date=="1/2/2007"|consumption$Date=="2/2/2007"), ]
+
+## Plot data - histogram
+hist(specConsumption$Global_active_power,main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",ylab="Frequency",col="red",
+     )
+
+## Export to .png
+dev.copy(png,file="plot1.png")
+dev.off()
