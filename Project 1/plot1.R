793c6d4717fd5e4e3f52d6223df405a5b342d0b1
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-05 14:21:23 -0500

    Add files via upload

--- Project 1/plot1.R
+++ Project 1/plot1.R
@@ -27,5 +27,5 @@ hist(specConsumption$Global_active_power,main="Global Active Power",
 ## )
 
 ## Export to .png
-dev.copy(png,file="plot1.png")
+dev.copy(png,file="plot1.png",height=480, width=480)
 dev.off()
