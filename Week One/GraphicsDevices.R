39733bc15c8e022e5452922611bd255de9708a6e
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-05 11:08:49 -0500

    Add files via upload

--- Week One/GraphicsDevices.R
+++ Week One/GraphicsDevices.R
@@ -8,3 +8,9 @@ title(main="Old Faithful Geyser Data")
 dev.off() ##Close PDF file device
 ##PDF creates file to computer. Will not show up on screen
 
+library(datasets)
+with(faithful,plot(eruptions,waiting)) ##create plot onscreen device
+title(main="Old Faithful Geyser Data") ##Main Title
+dev.copy(png,file="geyserplot.png") ##copy plot to .png file
+dev.off() ##CLOSE THE DEVICE! :)
+
