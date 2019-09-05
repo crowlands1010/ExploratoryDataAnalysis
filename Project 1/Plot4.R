8d5c731059cdc5db9668883a2626c57d28f44048
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-05 15:44:18 -0500

    Add files via upload

--- Project 1/Plot4.R
+++ Project 1/Plot4.R
@@ -24,13 +24,13 @@ consumptionDates$DateTime <- as.POSIXct(dateTime)
 par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,2,0))
 with(consumptionDates, {
   plot(Global_active_power~DateTime, type="l",xlab="",ylab="Global Active Power (kilowatts)")
-  plot(Voltage~DateTime, type="l",xlab="",ylab="Voltage (volt)")
-  plot(Sub_metering_1~DateTime, type="l",xlab="",ylab="Global Active Power (kilowatts)")
+  plot(Voltage~DateTime, type="l",xlab="datetime",ylab="Voltage (volt)")
+  plot(Sub_metering_1~DateTime, type="l",xlab="datetime",ylab="")
   lines(Sub_metering_2~DateTime,col='Red')
   lines(Sub_metering_3~DateTime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~DateTime, type="l",xlab="",ylab="Global Rective Power (kilowatts)") 
+  plot(Global_reactive_power~DateTime, type="l",xlab="datetime",ylab="Global_reactive_power") 
 })
 
 ## Export to .png
