06b89bead97babcbbeb1565c80f0040740e81c15
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-10 14:26:07 -0500

    Add files via upload

--- Week Two/ggplot2.R
+++ Week Two/ggplot2.R
@@ -4,17 +4,29 @@ library(ggplot2)
 str(mpg)
 
 ##compared to the "helloworld" of ggplot2
-qplot(displ,hwy,data=mpg) 
+qplot(displ,hwy,data = mpg) 
 
 ##add color variable to drv
-qplot(displ,hwy,data=mpg,color=drv) 
+qplot(displ,hwy,data = mpg,color = drv) 
 
 ##adding a geom
-qplot(displ,hwy,data=mpg,geom=c("point","smooth")) 
+qplot(displ,hwy,data = mpg,geom = c("point","smooth")) 
 ##histogram
-qplot(hwy,data=mpg,fill=drv)
+qplot(hwy,data = mpg,fill = drv)
 
 ##facets 
-qplot(displ,hwy,data=mpg,facets=.~drv)
-qplot(hwy,data=mpg,facets=drv~.,binwidth=2)
+qplot(displ,hwy,data = mpg,facets = .~drv)
+qplot(hwy,data = mpg,facets = drv~.,binwidth=2)
 
+##Notes about axis limits
+testdat <- data.frame(x=1:100,y=rnorm(100))
+testdat[50,2] <- 100 ##outlier
+plot(testdat$x,testdat$y,type = "l",ylim = c(-3,3))
+
+g <- ggplot(testdat,aes(x = x,y = y))
+
+g + geom_line()
+
+g + geom_line() + ylim(-3,3)
+
+g + geom_line() + coord_cartesian(ylim = c(-3,3))
