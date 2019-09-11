c503291cb7116050f1ee7ce10382a29403212ba4
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-09-11 09:06:31 -0500

    Add files via upload

--- Week Three/DimensionReduction.R
+++ Week Three/DimensionReduction.R
@@ -66,3 +66,50 @@ par(mfrow=c(1,3))
 image(t(constantMatrix)[,nrow(constantMatrix):1])
 plot(svd1$d,xlab="Column",ylab="Singular Value",pch=19)
 plot(svd1$d^2/sum(svd1$d^2),xlab="Column",ylab="Prop. of variance explained",pch=19)
+
+##What if we add a second pattern?
+set.seed(678910)
+for(i in 1:40){
+  ##flip a coin
+  coinFlip1 <- rbinom(1,size=1,prob=0.5)
+  coinFlip2 <- rbinom(1,size=1,prob=0.5)
+  ##if coin is heads, add a common pattern to that row
+  if (coinFlip1){
+    dataMatrix[i, ] < dataMatrix[1, ] + rep(c(0,5),each=5)
+  }
+  if (coinFlip2){
+    dataMatrix[i, ] <- dataMatrix[i, ] + rep(c(0,5),5)
+  }
+}
+hh <- hclust(dist(dataMatrix))
+dataMatrixOrdered <- dataMatrix[hh$order, ]
+
+##Singular value decomposition - true patterns
+svd2 <- svd(scale(dataMatrixOrdered))
+par(mfrow=c(1,3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1])
+plot(rep(c(0,1),each=5),pch=19,xlab="Column",ylab="Pattern 1")
+plot(rep(c(0,1),5),pch=19,xlab="Column",ylab="Pattern 2")
+
+##v and patterns of variance in rows
+svd2 <- svd(scale(dataMatrixOrdered))
+par(mfrow=c(1,3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1])
+plot(svd2$v[, 1],pch=19,xlab="Column",ylab="First right singular vector")
+plot(svd2$v[, 2],pch=19,xlab="Column",ylab="Second right singular vector")
+
+##d and variance explained
+svd1 <- svd(scale(dataMatrixOrdered))
+par(mfrow=c(1,2))
+plot(svd1$d,xlab="Column",ylab="Singular value",pch=19)
+plot(svd1$d^2/sum(svd1$d^2),xlab="Column",ylab="Percent of variance explained",
+     pch=19)
+
+##Imputing {impute}
+library(impute) ##http://www.bioconductor.org
+dataMatrix2 <- dataMatrixOrdered
+dataMatrix2[sample(1:100,size=40,replace=FALSE)] <- NA
+dataMatrix2 <- impute.knn(dataMatrix2)$data
+svd1 <- svd(scale(dataMatrixOrdered)); svd2 <- svd(scale(dataMatrix2))
+par(mfrow=c(1,2));plot(svd1$v[,1],pch=19); plot(svd2$v[,1],pch=19)
+
